b86f1054dd68d15cab2f3dcabe98c216efb3d6e3
kedesiklem <marin@elie.org>
2025-11-26 15:46:47 +0100

    fix : tag issue

--- files/scripts/purify_stone.lua
+++ files/scripts/purify_stone.lua
@@ -1,14 +1,17 @@
--- local log = dofile_once("mods/blankStone/utils/logger.lua") ---@type logger
+local log = dofile_once("mods/blankStone/utils/logger.lua") ---@type logger
 
 function material_area_checker_success(pos_x, pos_y)
+	log.debug("Purify stone activated")
 	local entity_id    = GetUpdatedEntityID()
+	log.debug("Entity ID: " .. tostring(entity_id))
 	for _,id in pairs(EntityGetInRadiusWithTag(pos_x, pos_y, 10, "item_pickup")) do
+		log.debug("Found item entity ID: " .. tostring(id))
 		-- make sure item is not carried in inventory or wand
 		if EntityGetRootEntity(id) == id then
 			local x,y = EntityGetTransform(entity_id)
 			EntityLoad("mods/blankStone/files/entities/blank_stone.xml", x, y - 5)
 			EntityLoad("data/entities/projectiles/explosion.xml", x, y - 10)
-			EntityKill(entity_id)
+			EntityKill(id)
 		end
 	end
 end
